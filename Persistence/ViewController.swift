650ae971f06176adaa921425aa941b6a38161839
Владимир Рыбалка <vrybalka@initium.ru>
2017-09-12 13:43:01 +0300

    Сериализация списков готово

--- Persistence/ViewController.swift
+++ Persistence/ViewController.swift
@@ -13,14 +13,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let fileUrl = dataFileUrl()
+        if(FileManager.default.fileExists(atPath: fileUrl.path)) {
+            if let array = NSArray(contentsOf: fileUrl) as? [String] {
+                for i in 0..<array.count {
+                    lineFieilds[i].text = array[i]
+                }
+            }
+        }
+        let app = UIApplication.shared
+        NotificationCenter.default.addObserver(self, selector: #selector(self.applicationWillResignActive(notification:)), name: Notification.Name.UIApplicationWillResignActive, object: app)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func applicationWillResignActive(notification: NSNotification) {
+        let fileUrl = self.dataFileUrl()
+        let array = (self.lineFieilds as NSArray).value(forKey: "text") as! NSArray
+        array.write(to: fileUrl, atomically: true)
+    }
 
-
+    func dataFileUrl() -> URL {
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        var url = URL(fileURLWithPath: "")
+        url = urls.first!.appendingPathComponent("data.plist")
+        
+        return url
+    }
 }
 
